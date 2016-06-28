3842b2308ac5971ecbabe1c2ea5cc8093a5dc03d
Qwert26 <qwert268862@gmail.com>
2016-06-28 13:45:59 +0200

    Dokumentation hinzugefügt

--- GruppenProjekt/GruppenProjekt/GruppenProjekt.py
+++ GruppenProjekt/GruppenProjekt/GruppenProjekt.py
@@ -3,22 +3,55 @@ import math
 from LCG import LCG
 #Parameters
 K=3
+"""Anzahl der Kassen bei Start"""
 L=5
+"""Anzahl der wartenden Kunden"""
 Tr=0.5 #Minuten
+"""Anzahl der Minuten pro Tier an der Kasse"""
 lamda=0.5 #/Minute
+"""Parameter der Exponentialverteilung"""
+
 def generate(enviroment):
     """Generiert Kunden"""
     while True:
-        enviroment.timeout();
-def inverseCDFExpo(x):
+        yield enviroment.timeout(lcg.nextTransformed(inverseCDFExponential));
+        enviroment.process(customer(enviroment,counters));
+def inverseCDFExponential(x):
+    """Inverse CDF der Exponentialverteilung. Liefert Zahlen im interval [0;+infinity)"""
     return -(math.log(1-x))/lamda;
 def noInSystem(resource):
+    """Aktuelle Anzahl der Nutzer einer Ressourcen"""
     return len(resource.users)+len(resource.queue);
 def customer(enviroment,ressources):
+    """Modelliert einen Kunden in der Tierhandlung."""
+    qLengths=[noInSystem(ressources[i]) for i in range(len(ressources))];
+    for i in range(len(ressources)):
+        if qLengths[i]==0 or qLengths[i]==min(qLengths):
+            choice=i;
+            break;
+    #kuerzeste Warteschlange gewaehlt.
+    res=ressources[choice];
+    with res.request() as req:
+        yield req;
+        tiere=1;
+        for i in range(4):
+            if lcg.nextBool(0.5):
+                tiere+=1;
+                #Binominalverteilte Inkrementierung: Zwischen +0 und +4.
+        yield enviroment.timeout(tiere*Tr+lcg.nextTransformed(inverseCDFPareto));
+    if noInSystem(res)==0:
+        ressources.remove(res);
     return;
+def inverseCDFPareto(x):
+    """inverse CDF der Paretoverteilung. Liefert Zahlen im Bereich [xmin;+infinity)."""
+    xmin=1.0;
+    alpha=2.0;
+    return xmin/((1-x)**(1/alpha))
+
 counters=[];
 lcg=LCG();
-env=simpy.Environment(8*60);
+env=simpy.Environment(8*60);#Starte die Simulation um 8 Uhr. Das sind 8*60 Minuten nach Mitternacht.
 for i in range(K):
     counters.append(simpy.Resource(env));
-noInSystem(counters[0]);
+env.process(generator);
+#env.run(until=16*60)#Lasse die Simulation bis 16 Uhr laufen. Das ist dann 16*60 Minuten nach Mitternacht.
